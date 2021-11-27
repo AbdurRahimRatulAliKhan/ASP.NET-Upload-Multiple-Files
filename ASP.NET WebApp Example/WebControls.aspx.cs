ac910e4a7ba7ac64ef78a84bd939f3610adb3595
Ratul Ali <rahim498617@gmail.com>
2021-11-27 10:17:33 +0600

    project uploaded

---
+++ ASP.NET WebApp Example/WebControls.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP.NET_WebApp_Example
+{
+    public partial class WebControls : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if ((FileUpload1.PostedFile != null) && (FileUpload1.PostedFile.ContentLength > 0))
+            {
+                var count = 0;
+                foreach (HttpPostedFile uploadedFile in FileUpload1.PostedFiles)
+                {
+
+
+                    string fn = System.IO.Path.GetFileName(uploadedFile.FileName);
+                    string SaveLocation = Server.MapPath("Upload") + "\\" + fn;
+
+                    try
+                    {
+                        uploadedFile.SaveAs(SaveLocation);
+                        count++;
+                    }
+
+                    catch (Exception ex)
+                    {
+                        FileUploadStatus.Text = "Error: " + ex.Message;
+                    }
+                }
+                if (count > 0)
+                {
+                    FileUploadStatus.Text = count + "files has been uploaded";
+                }
+            }
+            else
+            {
+                FileUploadStatus.Text = "Please select a file to upload.";
+            }
+        }
+    }
+}
